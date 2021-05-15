36043ee7e1e1a3a6b1308a0e0267b57598411c02
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-05-15 23:58:39 +0900

    2021_05_15

---
+++ Assets/Scripts/RotateShit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateShit : MonoBehaviour
+{
+    public GameObject Center;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(Center.transform.position, Vector3.down, Speed * Time.deltaTime);
+    }
+}
